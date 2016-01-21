3173a0ed3e240cf49a5fc28b87adc1a8750b126b
Nick Liebregts <n.liebregts@student.fontys.nl>
2016-01-21 15:45:14 +0100

    TestGetGame

--- MetacriticSite.Tests/MetaTest.cs
+++ MetacriticSite.Tests/MetaTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MetacriticSite.Klassen;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MetacriticSite.Tests
@@ -12,6 +13,16 @@ namespace MetacriticSite.Tests
             Klassen.Account acc = new Klassen.Account("test123");
             bool ac = acc.LogIn("test123", "Test123"); //looks if you can login with test123 / Test123
             Assert.AreEqual(true, ac, "Try to login");
+            bool fakeAcc = acc.LogIn("Fake", "Fake"); //fake account so can't login
+            Assert.AreEqual(false, fakeAcc, "Fake acc");
+        }
+
+        [TestMethod]
+        public void TestGetGame()
+        {
+            Game game = new Game(1, 10, "game", "genre", "some text", "xbox", DateTime.Now, 10);
+            game = game.getGame("Fallout4"); //get the game fallout4
+            Assert.AreEqual("Fallout4", game.Name, "Check if name is equal");
         }
     }
 }
